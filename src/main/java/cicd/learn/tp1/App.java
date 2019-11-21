1ae50d5fb972d177efa0ad7be9fa68b4f37945f8
guers <arthur.guers@etudiant.univ-lille1.fr>
2019-11-21 12:13:52 +0100

    PREMIERE_FEATURE Ajout d'un simley à l'affichage

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "CI/CD rocks!" );
+        System.out.print( "CI/CD rocks!" );
+        System.out.println(" :)");
     }
 }
